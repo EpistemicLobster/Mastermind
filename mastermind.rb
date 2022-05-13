f089c9845330bd3d93206e7ef375c21a3ed6f59c
EpistemicLobster <wikwemikong_kfz@aleeas.com>
2022-05-13 16:31:04 -0500

    added board display functionality; adherence to Ruby Style Guide

--- mastermind.rb
+++ mastermind.rb
@@ -1,80 +1,55 @@
 class Player
-  def initialize(name, role, choices)
+  def initialize(name, role, choice)
     @name = name
     @role = role
-    @choices = choices
+    @choice = choice
   end
 
-  def make_move
+  attr_accessor :choice, :role, :choice
 
+  def make_move(choices)
+    @choice = [choices]
   end
 end
 
 class Human < Player
-  make_choice
+  def make_move(choices)
+    puts 'Please input a for each location:'
+    puts 'Example: 1R 2B 3Y 4G'
+    super
+    @choice = gets.chomp.split(' ')
+  end
 end
 
 class Computer < Player
-
 end
 
 class Board
   def initialize
-    players = [Human.new('Human', '', Array.new), 
-               Computer.new('Computer', '', Array.new)]
-    board = Array.new(13) {Array.new(1, 'X X X X')}
-    CHOICES = "Choices: \n 'R', 'G', 'B', 'Y'"
+    @players = [Human.new('Human', '', []),
+               Computer.new('Computer', '', [])]
+    @game_board = { :Pegs => ['   Guesses    Turn   Key'], 12 => %w[X X X X], 
+              11 => %w[X X X X], 10 => %w[X X X X], 9 => %w[X X X X],
+              8 => %w[X X X X], 7 => %w[X X X X], 6 => %w[X X X X], 
+              5 => %w[X X X X], 4 => %w[X X X X], 3 => %w[X X X X],
+              2 => %w[X X X X], 1 => %w[X X X X] }
+    @choices = "Choices: 'R', 'G', 'B', 'Y'"
   end
+  attr_accessor :players, :game_board, :choices
 
-  def show(array = board)
-    board << Array.new(4, 'X')
+  def show(footer = choices)
+    game_board.each_pair do |k, v|
+      puts " #{v.join(' | ')}  #{k}"
+    end
+    puts footer
   end
 end
 
 class Game
-
 end
-
-board.each_with_index { |e, i| puts "#{puts e} # #{i} " if i > 0}
 
 # Correct Color & Position: Ͽ | Correct Color: Ͻ
 
-board = {1 =>'X X X X', 2 =>'X X X X', 3 =>'X X X X', 4 =>'X X X X',
-         5 =>'X X X X', 6 =>'X X X X', 7 =>'X X X X', 8 =>'X X X X',
-         9 =>'X X X X', 10 =>'X X X X', 11 =>'X X X X', 12 =>'X X X X',}
-i = 13
-x = board.transform_keys do |k|
-  k = i
-  i-=1
-end
-x = Hash.new()
+# Built method to show and display board properly
+# Built method to add new computer and human player
 
-board = {:Pegs => "   Turns       Key",
-  12=>"X X X X",          
-  11=>"X X X X",
-  10=>"X X X X",
-  9=>"X X X X",
-  8=>"X X X X",
-  7=>"X X X X",
-  6=>"X X X X",
-  5=>"X X X X",
-  4=>"X X X X",
-  3=>"X X X X",
-  2=>"X X X X",
-  1=>"X X X X"}
-
-  def output_hash(board, footer)
-    board.each_pair do |k, v|
-    puts  " #{v.join(' | ')}  #{k}"
-    end
-    puts footer
-    "Please make a choice."
-  end
-
-  output_hash(board, CHOICES)
-
-board = {:Pegs => ["   Guesses    Turn   Key"], 12=>%w(X X X X), 
-         11=>%w(X X X X), 10=>%w(X X X X), 9=>%w(X X X X),
-          8=>%w(X X X X), 7=>%w(X X X X), 6=>%w(X X X X), 
-          5=>%w(X X X X), 4=>%w(X X X X), 3=>%w(X X X X),
-          2=>%w(X X X X), 1=>%w(X X X X)}
